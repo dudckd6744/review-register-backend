bd5d07408a4353dd010eb4763004607d45025d78
우영창 <dudckd@uyeongchang-ui-MacBookPro.local>
2021-01-13 12:53:42 +0900

    modified

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
+const config = require("./config/key")
 
-mongoose.connect('mongodb+srv://wooang:01010@youtb.mdyrj.mongodb.net/relanidng?retryWrites=true&w=majority',{
+const { User } = require("./models/User")
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 app.get('/', (req,res) => res.send("hellow"))
+
+app.post('/api/register',(req,res)=>{
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo)=>{
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({success: true})
+    })
+})
 
 app.listen(port, ()=> console.log('Example app listenig on port...'))
